6869689ab2da788433fee0f0111572fb53eef41a
Michaelazzz <michaelavz1996@gmail.com>
2022-04-27 19:21:22 +0200

    chart panel interaction test

--- src/components/ChartPanel.tsx
+++ src/components/ChartPanel.tsx
@@ -8,41 +8,68 @@ import { Object3D, Raycaster, Vector2, Vector3 } from "three";
 
 Chart.register(BarController, BarElement, LinearScale, CategoryScale, Title);
 
-const ChartPanel = ({data}: any) => {
+const ChartObject = ({onMount}: any) => {
+
     const ref = useRef<Object3D>();
 
-    const { gl, scene } = useThree();
-    // let referenceSpace = gl.xr.getReferenceSpace();
-
-    let hover = false;
-
-    const rightController = useController("right");
+    const [textureData, setTextureData] = useState("");
 
     const loader = new THREE.TextureLoader();
 
-    var canvas = document.createElement("canvas");
-    // var ctx = canvas.getContext("2d");
-    canvas.width = 500;
-    canvas.height = 250;
-    let localCoord = new Vector2(0,0);
-    
-    let chart: Chart
-    
+    useEffect(() => {
+        onMount([textureData, setTextureData]);
+        loader.load(textureData, texture => {
+            // @ts-ignore
+            ref.current.set({backgroundTexture: texture});
+        });
+        console.log('texture update');
+    }, [onMount, textureData]);
+
+    return (
+        <block
+            ref={ref}
+            args={[
+                {
+                    width: 1,
+                    height: 0.5,
+                    fontSize: 0.1,
+                    backgroundOpacity: 1,
+                    fontFamily: "./Roboto-msdf.json",
+                    fontTexture: "./Roboto-msdf.png"
+                }
+            ]}
+        ></block>
+    );
+};
+
+const ChartPanel = ({data}: any) => {
+    const ref = useRef<Object3D>();
+    const { gl, scene } = useThree();
+    const rightController = useController("right");
+    let textureData = null;
+    let setTextureData:any = null;
     const raycaster = new Raycaster();
     let localPos = new Vector3(0,0,0);
-
     const geometry = new THREE.SphereGeometry( 0.01, 32, 16 );
     const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
     const sphere = new THREE.Mesh( geometry, material );
-    
+    let hover = false;
 
-    // canvas.addEventListener("mouseenter", (e) => {
-    //     console.log(e);
-    // });
+    var canvas = document.createElement("canvas");
+    canvas.width = 500;
+    canvas.height = 250;
+    const localCoord = new Vector2(0,0);
+
+    let chart: Chart
+
+    scene.add( sphere );
+
+    const onChildMount = (dataFromChild: any) => {
+        textureData = dataFromChild[0];
+        setTextureData = dataFromChild[1];
+    };
 
     useEffect(() => {
-        // draw on canvas
-        // if (ctx)
 
         chart = new Chart(canvas, {
             type: "bar",
@@ -50,24 +77,24 @@ const ChartPanel = ({data}: any) => {
                 labels: ["Red", "Orage", "Yellow", "Green", "Blue", "Purple", "Grey"],
                 datasets: [{
                     label: 'My First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    data: [65, 59, 80, 81, 56, 55, 60],
                     backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(255, 159, 64, 0.2)',
-                      'rgba(255, 205, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(201, 203, 207, 0.2)'
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
                     ],
                     borderColor: [
-                      'rgb(255, 99, 132)',
-                      'rgb(255, 159, 64)',
-                      'rgb(255, 205, 86)',
-                      'rgb(75, 192, 192)',
-                      'rgb(54, 162, 235)',
-                      'rgb(153, 102, 255)',
-                      'rgb(201, 203, 207)'
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
                     ],
                     borderWidth: 1
                 }]
@@ -95,59 +122,24 @@ const ChartPanel = ({data}: any) => {
                 },
                 onClick: (e) => {
                     // console.log(e);
-                    chart.ctx.fillStyle = "#000000";
-                    chart.ctx.beginPath();
-                    chart.ctx.arc(localCoord.x, localCoord.y, 8, 0, 2 * Math.PI);
-                    chart.ctx.fill();
-                    // chart.update();
                 },
                 onHover: (e) => {
                     // console.log(e);
-                    // chart.update();
                 },
                 events: ['mousemove', 'click']
             }
         });
-
         chart.update();
-        
-        loader.load(chart.toBase64Image(), texture => {
-            // @ts-ignore
-            ref.current.set({backgroundTexture: texture});
-        });
-        scene.add( sphere );
     });
 
 
     useXRFrame((time, xFrame) => {
 
-        // chart.update();
-
-        if(hover)
-        {
-            const event = new MouseEvent('mousemove', {
-                clientX: localCoord.x,
-                clientY: localCoord.y
-            });
-    
-            chart.canvas.dispatchEvent(event);
-        }
-
         if (!ref.current) 
             return;
-
-        loader.load(chart.toBase64Image(), texture => {
-            // @ts-ignore
-            ref.current.set({backgroundTexture: texture});
-        });
-
-        if(hover && rightController) 
+        
+        if(rightController && hover) 
         {
-            // let targetRayPose = xFrame.getPose(rightController.inputSource.targetRaySpace, referenceSpace); // transform corresponding to the target ray pose
-            // let targetRayOrigin = new Vector3(targetRayPose?.transform.position.x, targetRayPose?.transform.position.y, targetRayPose?.transform.position.z); // origin point of the ray, at the front of the controller
-            // let targetRayVector = new Vector3(targetRayPose?.transform.orientation.x, targetRayPose?.transform.orientation.y, targetRayPose?.transform.orientation.z); // direction the ray is pointing
-            // targetRayVector.add(new Vector3(0,0,-1));
-
             let targetRay = new Vector3();
             rightController.controller.getWorldDirection(targetRay).negate();
             let position = new Vector3();
@@ -156,27 +148,34 @@ const ChartPanel = ({data}: any) => {
             raycaster.set(position, targetRay);
         
             let intersection = raycaster.intersectObject(ref.current as Object3D);
+           
 
             if(!intersection[0])
             {
-                sphere.position.copy(new Vector3(0,0,0));
+                return;
             }
-            else 
-            {
-                sphere.position.copy(intersection[0].point);
-                localPos.copy(ref.current.clone().worldToLocal(intersection[0].point)); // converts point in world space to local space
-                localCoord.setX(500*(localPos.x+0.5));
-                localCoord.setY(canvas.height*Math.abs(localPos.y-0.25));
-            }  
+            
+            const event = new MouseEvent('mousemove', {
+                clientX: localCoord.x,
+                clientY: localCoord.y
+            });
+    
+            chart.canvas.dispatchEvent(event);
+
+            sphere.position.copy(intersection[0].point);
+            localPos.copy(ref.current.worldToLocal(intersection[0].point)); // converts point in world space to local space
+            localCoord.setX(500*(localPos.x+0.5));
+            localCoord.setY(canvas.height*Math.abs(localPos.y-0.25));
         }
         else 
         {
             sphere.position.copy(new Vector3(0,0,0));
         }
+
+        setTextureData(chart.toBase64Image());
     });
 
     useInteraction(ref, 'onSelect', () => {
-
         const event = new MouseEvent('click', {
             clientX: localCoord.x,
             clientY: localCoord.y
@@ -201,12 +200,18 @@ const ChartPanel = ({data}: any) => {
                     width: 1,
                     height: 0.5,
                     fontSize: 0.1,
-                    backgroundOpacity: 1,
+                    backgroundOpacity: 0,
                     fontFamily: "./Roboto-msdf.json",
                     fontTexture: "./Roboto-msdf.png"
                 }
             ]}
-        ></block>
+        >
+            <ChartObject
+                // textureData={textureData}
+                onMount={onChildMount}
+            ></ChartObject>
+        </block>
+        
     );
 };
 
